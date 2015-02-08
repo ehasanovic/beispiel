b1cb15a440186cd4f2845baadeb9ffb03366f070
kasia <kasia@kasia-laptop>
2015-02-08 21:17:01 +0100

    getter und setter in der klasse auto

--- Beispiel/src/beispiel/Auto.java
+++ Beispiel/src/beispiel/Auto.java
@@ -13,4 +13,11 @@ public class Auto
 		String a;
 		return "Auto: " + name + 100;
 	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 }
